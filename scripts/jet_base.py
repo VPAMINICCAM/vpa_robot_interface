c8f3d8aeda0b22b7bb938ae19453009ffe904228
nano <yikai.zeng@tu-dresden.de>
2024-03-18 18:07:53 +0100

    first working commit

--- scripts/jet_base.py
+++ scripts/jet_base.py
@@ -2,47 +2,23 @@
 
 import rospy
 
-from ackermann_msgs.msg import AckermannDrive
-from vpa_robot_interface.msg import jetracer_act
+# from ackermann_msgs.msg import AckermannDrive
+from vpa_robot_interface.msg import JetServo
+#from std_msgs.msg import Float32
 from jetracer.nvidia_racecar import NvidiaRacecar
 
-class jetracer_driver:
+car = NvidiaRacecar()
+car.steering_offset = 0.12
+
+def callback(data):
+    car.steering = data.steering_servo
+    car.throttle = data.throttle
+    # print('steering:',car.steering,'throttle:',car.throttle)
     
-    def __init__(self) -> None:
-        self.car = NvidiaRacecar()
-        
-        self.car.steering_offset = 0.12
-        self.car.steering = 0
-        self.car.throttle = 0
-        
-        self.cmd_sub = rospy.Subscriber('/cmd_jetact',jetracer_act,self._cb,queue_size=1)
-        
-    def _cb(self,msg:jetracer_act):
-        self.car.steering = msg.steering_servo
-        self.car.throttle = msg.throttle
-        # # positive to the left
-        
-        # servo_output = -(_steering_angle * 57.2958)/15
-        # if servo_output > 1:
-        #     servo_output = 1
-        # elif servo_output < -1:
-        #     servo_output = -1
-        
-        # self.car.steering = servo_output
-        # _acc = msg.acceleration # m/s^2
-        
-        # # but this is rough with not close loop
-        
-        # if _acc > 1:
-        #     _acc = 1
-        # elif _acc < -1:
-        #     _acc = -1
-        # self.car.throttle = _acc
-        
-if __name__ == "__main__":
-    try:
-        rospy.init_node('jet_base')
-        T = jetracer_driver()
-        rospy.spin()
-    except KeyboardInterrupt:
-        print('shutdown')
+def listener():
+    rospy.init_node('tutorials_msg_sub', anonymous=False)
+    rospy.Subscriber("cmd", JetServo, callback)
+    rospy.spin()
+    
+if __name__ == '__main__':
+    listener()
