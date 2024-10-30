a68f5d78e3bc8b79bc6cea036e777166d8ed8807
Yikai Zeng <yikai.zeng@tu-dresden.de>
2024-10-30 10:19:14 +0100

    set usart rights

--- initenv.sh
+++ initenv.sh
@@ -6,6 +6,8 @@ echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MOD
 
 echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="2e3c", ATTRS{idProduct}=="5740", MODE:="0777", GROUP:="dialout",  SYMLINK+="nvilidar"' >/etc/udev/rules.d/nvilidar_usb.rules
 
+echo  'KERNEL=="ttyS0", MODE:="0666", GROUP:="dialout",  SYMLINK+="move_base"' >/etc/udev/rules.d/move_base_pi4.rules
+
 systemctl daemon-reload
 service udev reload
 sleep 2
