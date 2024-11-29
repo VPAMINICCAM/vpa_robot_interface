fe9e8361ee5203e3f4b34dfe05ae46343700b4cd
vpaadmin <vpa-snamlab@tu-dresden.de>
2024-11-29 18:17:44 +0100

    pi contronller add ki calc

--- scripts/pid_controller/pi_format.py
+++ scripts/pid_controller/pi_format.py
@@ -2,41 +2,40 @@
 
 class PI_controller:
 
-    # This is an incremental PID control
+    """ A class to implement a PI (Proportional-Intergral) controller """
 
-    def __init__(self,kp,ki) -> None:
-        
+    def __init__(self, kp, ki) -> None:
         self.kp = kp
         self.ki = ki
-        self.control_output = 0
-        self.err_record     = 0
+        self.frequency = 20
+        self.dt = 1 / self.frequency
 
-    def record_current_error(self,err) -> None:
-        self.err_record = err
+        self.last_error = 0
+        self.last_output     = 0
 
-    def pi_control(self,ref,sig) -> float:
-        
+    def pi_control(self, ref, sig) -> float:
         if ref == 0:
-            self.reset_controller()
+            self.reset()
             return 0
         
+        # current err
         err = ref - sig
-        delta_err = err - self.err_record
+
+        delta_err = err - self.last_error
         
-        self.record_current_error(err)
+        delta_output = self.kp * delta_err + self.ki * self.dt * err
 
-        delta_output = self.kp * delta_err + self.ki * err
+        output = self.last_output + delta_output
 
-        self.control_output += delta_output
+        self.last_error = err
+        self.last_output = output
 
-        return self.control_output
+        return output
     
     def update_controller_param(self,kp,ki) -> None:
-
         self.kp = kp
         self.ki = ki
         
-    def reset_controller(self):
-        
-        self.control_output = 0
-        self.err_record     = 0       
+    def reset(self):        
+        self.last_error = 0
+        self.last_output = 0       
