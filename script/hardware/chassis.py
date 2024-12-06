e4558ed3701224570b5f078bc50efa6f2dab57db
Yikai Zeng <yikai.zeng@tu-dresden.de>
2024-12-06 18:17:22 +0100

    correct linear speed to round per sec

--- script/hardware/chassis.py
+++ script/hardware/chassis.py
@@ -5,5 +5,5 @@ class CHASSIS:
     def calculate_wheel_speeds(self, linear_x: float):
         """Calculate wheel speeds in rps."""
         if linear_x != 0:
-            return linear_x / (2 * 3.14 * self.wheel_diameter)
+            return linear_x / (3.14 * self.wheel_diameter)
         return 0
