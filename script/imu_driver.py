1edd574f51390d67a9b61e8c74b122fd5938ef1d
Yikai Zeng <yikai.zeng@tu-dresden.de>
2024-12-23 15:09:16 +0100

    update imu node, align with other packages

--- script/imu_driver.py
+++ script/imu_driver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-# this script based on: https://github.com/OSUrobotics/mpu_6050_driver/blob/master/scripts/imu_node.py 
+# Refactored for better object-oriented structure
 
+from enum import Enum
 import smbus
 import socket
 import rospy
@@ -8,98 +9,131 @@ import numpy as np
 from sensor_msgs.msg import Temperature, Imu
 from std_msgs.msg import Bool
 from tf.transformations import quaternion_about_axis
-from mpu_6050_driver.registers import PWR_MGMT_1, ACCEL_XOUT_H, ACCEL_YOUT_H, ACCEL_ZOUT_H, TEMP_H,\
-    GYRO_XOUT_H, GYRO_YOUT_H, GYRO_ZOUT_H
 
-ADDR = None
-bus = None
-IMU_FRAME = None
+class MPU6050Registers(Enum):
+    CONFIG = 0x1A
+    PWR_MGMT_1 = 0x6B
+    ACCEL_CONFIG = 0x1C
+    ACCEL_XOUT_H = 0x3B
+    ACCEL_XOUT_L = 0x3C
+    ACCEL_YOUT_H = 0x3D
+    ACCEL_YOUT_L = 0x3E
+    ACCEL_ZOUT_H = 0x3F
+    ACCEL_ZOUT_L = 0x40
+    GYRO_CONFIG = 0x1B
+    GYRO_XOUT_H = 0x43
+    GYRO_XOUT_L = 0x44
+    GYRO_YOUT_H = 0x45
+    GYRO_YOUT_L = 0x46
+    GYRO_ZOUT_H = 0x47
+    GYRO_ZOUT_L = 0x48
+    TEMP_H = 0x41
+    TEMP_L = 0x42
 
-# read_word and read_word_2c from http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
-def read_word(adr):
-    high = bus.read_byte_data(ADDR, adr)
-    low = bus.read_byte_data(ADDR, adr+1)
-    val = (high << 8) + low
-    return val
 
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
+class MPU6050Node:
+    def __init__(self):
+        rospy.init_node('imu_node')
+
+        self.robot_name = socket.gethostname()
+        self.bus = smbus.SMBus(rospy.get_param('~bus', 6))
+        self.addr = self._get_device_address(rospy.get_param('~device_address', 0x68))
+        self.imu_frame = rospy.get_param('~imu_frame', 'imu_link')
+        self.publish_freq = rospy.get_param('~imu_pub_freq', 20)
+
+        self.dlpf_cfg = rospy.get_param('~dlpf_cfg', 2)  # Default to 92Hz bandwidth
+
+        self.temp_pub = rospy.Publisher('temperature', Temperature, queue_size=1)
+        self.imu_pub = rospy.Publisher('imu', Imu, queue_size=1)
+
+        # Initialize MPU6050
+        self.bus.write_byte_data(self.addr, MPU6050Registers.PWR_MGMT_1.value, 0)
+        self.set_dlpf(self.dlpf_cfg)
+
+        # Start publishing timers
+        self.imu_timer = rospy.Timer(rospy.Duration(1 / self.publish_freq), self.publish_imu)
+        self.temp_timer = rospy.Timer(rospy.Duration(10), self.publish_temp)
+
+        rospy.on_shutdown(self.cleanup)
+
+        rospy.loginfo(f"{self.robot_name}: IMU sensor ready")
+
+    @staticmethod
+    def _get_device_address(addr):
+        if isinstance(addr, str):
+            return int(addr, 16)
+        return addr
+
+    def set_dlpf(self, dlpf_cfg):
+        """Set the Digital Low Pass Filter configuration."""
+        if not (0 <= dlpf_cfg <= 6):
+            rospy.logwarn(f"Invalid DLPF_CFG value {dlpf_cfg}. Must be between 0 and 6. Defaulting to 2.")
+            dlpf_cfg = 2
+        self.bus.write_byte_data(self.addr, MPU6050Registers.CONFIG.value, dlpf_cfg)
+        rospy.loginfo(f"DLPF set to configuration {dlpf_cfg}.")
+
+    def read_word(self, register):
+        high = self.bus.read_byte_data(self.addr, register.value)
+        low = self.bus.read_byte_data(self.addr, register.value + 1)
+        return (high << 8) + low
+
+    def read_word_2c(self, register):
+        val = self.read_word(register)
+        if val >= 0x8000:
+            return -((65535 - val) + 1)
         return val
 
-def publish_temp(timer_event):
-    temp_msg = Temperature()
-    temp_msg.header.frame_id = IMU_FRAME
-    temp_msg.temperature = read_word_2c(TEMP_H)/340.0 + 36.53
-    temp_msg.header.stamp = rospy.Time.now()
-    temp_pub.publish(temp_msg)
+    def publish_temp(self, timer_event):
+        temp_msg = Temperature()
+        temp_msg.header.frame_id = self.imu_frame
+        temp_msg.temperature = self.read_word_2c(MPU6050Registers.TEMP_H) / 340.0 + 36.53
+        temp_msg.header.stamp = rospy.Time.now()
+        self.temp_pub.publish(temp_msg)
 
+    def publish_imu(self, timer_event):
+        imu_msg = Imu()
+        imu_msg.header.frame_id = self.imu_frame
 
-def publish_imu(timer_event):
-    imu_msg = Imu()
-    imu_msg.header.frame_id = IMU_FRAME
+        # Read acceleration values
+        accel_x = self.read_word_2c(MPU6050Registers.ACCEL_XOUT_H) / 16384.0
+        accel_y = self.read_word_2c(MPU6050Registers.ACCEL_YOUT_H) / 16384.0
+        accel_z = self.read_word_2c(MPU6050Registers.ACCEL_ZOUT_H) / 16384.0
 
-    # Read the acceleration vals
-    accel_x = read_word_2c(ACCEL_XOUT_H) / 16384.0
-    accel_y = read_word_2c(ACCEL_YOUT_H) / 16384.0
-    accel_z = read_word_2c(ACCEL_ZOUT_H) / 16384.0
-    
-    # Calculate a quaternion representing the orientation
-    accel = accel_x, accel_y, accel_z
-    ref = np.array([0, 0, 1])
-    acceln = accel / np.linalg.norm(accel)
-    axis = np.cross(acceln, ref)
-    angle = np.arccos(np.dot(acceln, ref))
-    orientation = quaternion_about_axis(angle, axis)
+        # Calculate orientation quaternion
+        accel = np.array([accel_x, accel_y, accel_z])
+        ref = np.array([0, 0, 1])
+        acceln = accel / np.linalg.norm(accel)
+        axis = np.cross(acceln, ref)
+        angle = np.arccos(np.dot(acceln, ref))
+        orientation = quaternion_about_axis(angle, axis)
 
-    # Read the gyro vals
-    gyro_x = read_word_2c(GYRO_XOUT_H) / 131.0
-    gyro_y = read_word_2c(GYRO_YOUT_H) / 131.0
-    gyro_z = read_word_2c(GYRO_ZOUT_H) / 131.0
-    
-    # Load up the IMU message
-    o = imu_msg.orientation
-    o.x, o.y, o.z, o.w = orientation
+        # Read gyro values
+        gyro_x = self.read_word_2c(MPU6050Registers.GYRO_XOUT_H) / 131.0 * (np.pi / 180)
+        gyro_y = self.read_word_2c(MPU6050Registers.GYRO_YOUT_H) / 131.0 * (np.pi / 180)
+        gyro_z = self.read_word_2c(MPU6050Registers.GYRO_ZOUT_H) / 131.0 * (np.pi / 180)
 
-    imu_msg.linear_acceleration.x = accel_x
-    imu_msg.linear_acceleration.y = accel_y
-    imu_msg.linear_acceleration.z = accel_z
+        # Populate IMU message
+        imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w = orientation
+        imu_msg.linear_acceleration.x = accel_x
+        imu_msg.linear_acceleration.y = accel_y
+        imu_msg.linear_acceleration.z = accel_z
+        imu_msg.angular_velocity.x = gyro_x
+        imu_msg.angular_velocity.y = gyro_y
+        imu_msg.angular_velocity.z = gyro_z
+        imu_msg.header.stamp = rospy.Time.now()
 
-    imu_msg.angular_velocity.x = gyro_x
-    imu_msg.angular_velocity.y = gyro_y
-    imu_msg.angular_velocity.z = gyro_z
+        self.imu_pub.publish(imu_msg)
 
-    imu_msg.header.stamp = rospy.Time.now()
+    def run(self):
+        rospy.spin()
 
-    imu_pub.publish(imu_msg)
+    def cleanup(self):
+        """Cleanup resources on shutdown."""
+        rospy.loginfo(f"{self.robot_name}: Cleaning up resources.")
+        self.imu_timer.shutdown()
+        self.temp_timer.shutdown()
 
-def signal_shut(msg:Bool):
-    if msg.data:
-        rospy.signal_shutdown('IMU sensor node shutdown')
-
-
-temp_pub = None
-imu_pub = None
 
 if __name__ == '__main__':
-    rospy.init_node('imu_node')
-    rospy.Subscriber("robot_interface_shutdown", Bool,signal_shut)
-    robot_name = socket.gethostname()
-    bus     = smbus.SMBus(rospy.get_param('~bus', 6))
-    ADDR    = rospy.get_param('~device_address', 0x68)
-    if type(ADDR) == str:
-        ADDR = int(ADDR, 16)
-    
-    publish_freq    = rospy.get_param('~imu_pub_freq', 20)
-    IMU_FRAME       = rospy.get_param('~imu_frame', 'imu_link')
-
-    bus.write_byte_data(ADDR, PWR_MGMT_1, 0)
-
-    temp_pub    = rospy.Publisher('temperature', Temperature,queue_size=1)
-    imu_pub     = rospy.Publisher('imu', Imu,queue_size=1)
-    imu_timer   = rospy.Timer(rospy.Duration(1/20), publish_imu)
-    temp_timer  = rospy.Timer(rospy.Duration(10), publish_temp)
-    rospy.loginfo("%s: imu sensor ready",robot_name)
-    rospy.spin()
+    node = MPU6050Node()
+    node.run()
