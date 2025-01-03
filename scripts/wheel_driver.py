2c6e060335688b9daeb97cff640b0e11f5ce033b
Yikai Zeng <yikai.zeng@tu-dresden.de>
2025-01-03 15:47:30 +0100

    reverse z axis tf

--- scripts/wheel_driver.py
+++ scripts/wheel_driver.py
@@ -190,7 +190,7 @@ class WheelDriverNode:
     def car_cmd_cb(self,msg_car_cmd:Twist) -> None:
         msg_car_cmd.linear.x    = max(min(msg_car_cmd.linear.x,self._v_max),-self._v_max)
         msg_car_cmd.angular.z   = max(min(msg_car_cmd.angular.z,self._omega_max),-self._omega_max)
-        self.yaw_setpoint = msg_car_cmd.angular.z
+        self.yaw_setpoint = -msg_car_cmd.angular.z  # Negate the yaw setpoint
         if not self.estop:
             self.omega_right_ref    = ((msg_car_cmd.linear.x + 0.5 * msg_car_cmd.angular.z * self._baseline) / self._radius) * (1 + self.trim)
             self.omega_left_ref     = ((msg_car_cmd.linear.x - 0.5 * msg_car_cmd.angular.z * self._baseline) / self._radius) * (1 - self.trim)
